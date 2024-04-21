3d98b8e23ae30d3d3faa64cf08b84d12980400e0
Sheila <sajbalboa@hotmail.com>
2024-04-21 09:30:36 -0500

    Dashboard creado. Falta funcionalidad

--- Televecino_ultima version/dashmin-1.0.0/js/main.js
+++ Televecino_ultima version/dashmin-1.0.0/js/main.js
@@ -59,26 +59,31 @@
     });
 
 
-    // Worldwide Sales Chart
-    var ctx1 = $("#worldwide-sales").get(0).getContext("2d");
+    // Tipos de incidencias - Chart
+    var ctx1 = $("#type-incidents-bar").get(0).getContext("2d");
     var myChart1 = new Chart(ctx1, {
         type: "bar",
         data: {
-            labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
+            labels: ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sábado", "Domingo"],
             datasets: [{
-                    label: "USA",
-                    data: [15, 30, 55, 65, 60, 80, 95],
-                    backgroundColor: "rgba(0, 156, 255, .7)"
+                    label: "Seguridad pública",
+                    data: [20, 33, 14, 25, 38, 11, 29],
+                    backgroundColor: "rgba(255, 183, 3, 1)"
                 },
                 {
-                    label: "UK",
-                    data: [8, 35, 40, 60, 70, 55, 75],
-                    backgroundColor: "rgba(0, 156, 255, .5)"
+                    label: "Emergencia médica",
+                    data: [23, 31, 17, 38, 29, 12, 35],
+                    backgroundColor: "rgba(251, 133, 0, 1)"
                 },
                 {
-                    label: "AU",
-                    data: [12, 25, 45, 55, 65, 70, 60],
-                    backgroundColor: "rgba(0, 156, 255, .3)"
+                    label: "Infraestructura",
+                    data: [28, 36, 15, 32, 19, 27, 10],
+                    backgroundColor: "rgba(33, 158, 188, 1)"
+                },
+                {
+                    label: "Otro",
+                    data: [16, 30, 25, 12, 37, 22, 18],
+                    backgroundColor: "rgba(2, 48, 71, 1)"
                 }
             ]
             },
@@ -86,25 +91,41 @@
             responsive: true
         }
     });
-
-
-    // Salse & Revenue Chart
-    var ctx2 = $("#salse-revenue").get(0).getContext("2d");
+    // Estados de incidencias - Chart
+    var ctx2 = $("#status-incidents").get(0).getContext("2d");
     var myChart2 = new Chart(ctx2, {
         type: "line",
         data: {
-            labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
+            labels: ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sábado", "Domingo"],
             datasets: [{
-                    label: "Salse",
-                    data: [15, 30, 55, 45, 70, 65, 85],
-                    backgroundColor: "rgba(0, 156, 255, .5)",
-                    fill: true
+                    label: "Procesado",
+                    data: [17, 26, 14, 21, 25, 14, 18],
+                    backgroundColor: "rgba(42, 157, 143, 1)",
+                    fill: false
                 },
                 {
-                    label: "Revenue",
-                    data: [99, 135, 170, 130, 190, 180, 270],
-                    backgroundColor: "rgba(0, 156, 255, .3)",
-                    fill: true
+                    label: "Pendiente",
+                    data: [22, 32, 18, 27, 31, 18, 23],
+                    backgroundColor:"rgba(233, 196, 106, 1)",
+                    fill: false
+                },
+                {
+                    label: "En curso",
+                    data: [26, 39, 21, 32, 37, 22, 28],
+                    backgroundColor: "rgba(38, 70, 83, 1)",
+                    fill: false
+                },
+                {
+                    label: "Cancelado",
+                    data: [13, 20, 11, 16, 18, 11, 14],
+                    backgroundColor:"rgba(231, 111, 81, 1)",
+                    fill: false
+                },
+                {
+                    label: "Rechazado",
+                    data: [9, 13, 7, 11, 12, 7, 9],
+                    backgroundColor: "rgba(244, 162, 97, 1)",
+                    fill: false
                 }
             ]
             },
@@ -112,12 +133,94 @@
             responsive: true
         }
     });
-    
-
-
-    // Single Line Chart
-    var ctx3 = $("#line-chart").get(0).getContext("2d");
+    // Tipo 1
+    var ctx3 = $("#type-incidents-pie-1").get(0).getContext("2d");
     var myChart3 = new Chart(ctx3, {
+        type: "pie",
+        data: {
+            labels: ["Procesado - 20%","Pendiente - 25%", "En curso - 30%", "Cancelado - 15%","Rechazado - 10%"],
+            datasets: [{
+                backgroundColor: [
+                    "rgba(42, 157, 143, 1)",
+                    "rgba(233, 196, 106, 1)",
+                    "rgba(38, 70, 83, 1)",
+                    "rgba(231, 111, 81, 1)",
+                    "rgba(244, 162, 97, 1)"
+                ],
+                data: [20,25,30,15,10],
+            }]
+        },
+        options: {
+            responsive: true
+        }
+    });
+    // Tipo 2
+    var ctx4 = $("#type-incidents-pie-2").get(0).getContext("2d");
+    var myChart4 = new Chart(ctx4, {
+        type: "pie",
+        data: {
+            labels: ["Procesado - 20%","Pendiente - 25%", "En curso - 30%", "Cancelado - 15%","Rechazado - 10%"],
+            datasets: [{
+                backgroundColor: [
+                    "rgba(42, 157, 143, 1)",
+                    "rgba(233, 196, 106, 1)",
+                    "rgba(38, 70, 83, 1)",
+                    "rgba(231, 111, 81, 1)",
+                    "rgba(244, 162, 97, 1)"
+                ],
+                data: [20,25,30,15,10],
+            }]
+        },
+        options: {
+            responsive: true
+        }
+    });
+    // Tipo 3
+    var ctx5 = $("#type-incidents-pie-3").get(0).getContext("2d");
+    var myChart5 = new Chart(ctx5, {
+        type: "pie",
+        data: {
+            labels: ["Procesado - 20%","Pendiente - 25%", "En curso - 30%", "Cancelado - 15%","Rechazado - 10%"],
+            datasets: [{
+                backgroundColor: [
+                    "rgba(42, 157, 143, 1)",
+                    "rgba(233, 196, 106, 1)",
+                    "rgba(38, 70, 83, 1)",
+                    "rgba(231, 111, 81, 1)",
+                    "rgba(244, 162, 97, 1)"
+                ],
+                data: [20,25,30,15,10],
+            }]
+        },
+        options: {
+            responsive: true
+        }
+    });
+    // Tipo 4
+    var ctx6 = $("#type-incidents-pie-4").get(0).getContext("2d");
+    var myChart6 = new Chart(ctx6, {
+        type: "pie",
+        data: {
+            labels: ["Procesado - 20%","Pendiente - 25%", "En curso - 30%", "Cancelado - 15%","Rechazado - 10%"],
+            datasets: [{
+                backgroundColor: [
+                    "rgba(42, 157, 143, 1)",
+                    "rgba(233, 196, 106, 1)",
+                    "rgba(38, 70, 83, 1)",
+                    "rgba(231, 111, 81, 1)",
+                    "rgba(244, 162, 97, 1)"
+                ],
+                data: [20,25,30,15,10],
+            }]
+        },
+        options: {
+            responsive: true
+        }
+    });
+/*
+    // Single Line Chart
+    var ctx7 = $("#line-chart").get(0).getContext("2d");
+    var myChart7 = new Chart(ctx7, {
         type: "line",
         data: {
             labels: [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150],
@@ -132,11 +235,9 @@
             responsive: true
         }
     });
-
-
     // Single Bar Chart
-    var ctx4 = $("#bar-chart").get(0).getContext("2d");
-    var myChart4 = new Chart(ctx4, {
+    var ctx8 = $("#bar-chart").get(0).getContext("2d");
+    var myChart8 = new Chart(ctx8, {
         type: "bar",
         data: {
             labels: ["Italy", "France", "Spain", "USA", "Argentina"],
@@ -155,34 +256,9 @@
             responsive: true
         }
     });
-
-
-    // Pie Chart
-    var ctx5 = $("#pie-chart").get(0).getContext("2d");
-    var myChart5 = new Chart(ctx5, {
-        type: "pie",
-        data: {
-            labels: ["Italy", "France", "Spain", "USA", "Argentina"],
-            datasets: [{
-                backgroundColor: [
-                    "rgba(0, 156, 255, .7)",
-                    "rgba(0, 156, 255, .6)",
-                    "rgba(0, 156, 255, .5)",
-                    "rgba(0, 156, 255, .4)",
-                    "rgba(0, 156, 255, .3)"
-                ],
-                data: [55, 49, 44, 24, 15]
-            }]
-        },
-        options: {
-            responsive: true
-        }
-    });
-
-
     // Doughnut Chart
-    var ctx6 = $("#doughnut-chart").get(0).getContext("2d");
-    var myChart6 = new Chart(ctx6, {
+    var ctx9 = $("#doughnut-chart").get(0).getContext("2d");
+    var myChart9 = new Chart(ctx9, {
         type: "doughnut",
         data: {
             labels: ["Italy", "France", "Spain", "USA", "Argentina"],
@@ -201,7 +277,7 @@
             responsive: true
         }
     });
-
+    */
     
 })(jQuery);
 
