d95ba632de4b5d8dfb493b187fd8048b27d78001
Jesus Alvarado <a20211688@pucp.edu.pe>
2024-05-27 17:19:52 -0500

    avance

--- TelevecinosUnidos_AppWeb/src/main/java/org/example/televecinosunidos_appweb/servlets/SerenazgoServlet.java
+++ TelevecinosUnidos_AppWeb/src/main/java/org/example/televecinosunidos_appweb/servlets/SerenazgoServlet.java
@@ -10,7 +10,9 @@ import java.io.IOException;
 public class SerenazgoServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String vista = "/WEB-INF/inicioSerenazgo.jsp";
+        RequestDispatcher rd = request.getRequestDispatcher(vista);//le pongo la información
+        rd.forward(request,response);//vista comienza  a correr
     }
 
     @Override
--- TelevecinosUnidos_AppWeb/src/main/webapp/index.jsp
+++ TelevecinosUnidos_AppWeb/src/main/webapp/index.jsp
@@ -5,7 +5,7 @@
     <title>JSP - Hello World</title>
 </head>
 <body>
-<h1><%= "Hello World!" %>
+<h1><%= "aaaa!" %>
 </h1>
 <br/>
 <a href="hello-servlet">Hello Servlet</a>
