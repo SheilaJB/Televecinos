c554b8a1e2980e74db0e6c8baa8f8a02aec0372a
Jostin <a20213830@pucp.edu.pe>
2024-05-28 01:28:20 -0500

    Turno Serenazgo

---
+++ TelevecinosUnidos_AppWeb/src/main/java/org/example/televecinosunidos_appweb/model/beans/TurnoSerenazgoB.java
@@ -0,0 +1,23 @@
+package org.example.televecinosunidos_appweb.model.beans;
+
+public class TurnoSerenazgoB {
+    private int idTurnoSerenazgo;
+
+    private String turno;
+
+    public int getIdTurnoSerenazgo() {
+        return idTurnoSerenazgo;
+    }
+
+    public void setIdTurnoSerenazgo(int idTurnoSerenazgo) {
+        this.idTurnoSerenazgo = idTurnoSerenazgo;
+    }
+
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+}
