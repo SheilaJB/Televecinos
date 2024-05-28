9c48b07e8e190c5422a75ace696c47c7526b1efb
Jesus Alvarado <a20211688@pucp.edu.pe>
2024-05-27 22:35:47 -0500

    servlets modificacion

--- TelevecinosUnidos_AppWeb/src/main/java/org/example/televecinosunidos_appweb/servlets/CoordinadorServlet.java
+++ TelevecinosUnidos_AppWeb/src/main/java/org/example/televecinosunidos_appweb/servlets/CoordinadorServlet.java
@@ -1,4 +1,20 @@
 package org.example.televecinosunidos_appweb.servlets;
 
-public class CoordinadorServlet {
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "CoordinadorServlet", value = "/CoordinadorServlet")
+public class CoordinadorServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
 }
