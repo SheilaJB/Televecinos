af2956e10ad83e52d7969495c947baf6a1e31056
Jesus Alvarado <a20211688@pucp.edu.pe>
2024-06-16 04:58:44 -0500

    actualizacion llenado de datos para hacer pruebas en admin

--- Base_de_datos_Televecinos/llenarDatos.sql
+++ Base_de_datos_Televecinos/llenarDatos.sql
@@ -106,7 +106,7 @@ INSERT INTO `televecinosDB`.`TurnoSerenazgo` (`idTurnoSerenazgo`,`turno`) VALUES
 INSERT INTO `televecinosDB`.`TipoSerenazgo` (`idTipoSerenazgo`,`tipo`) VALUES
 (1,'Bicicleta'), (2,'A pie'), (3,'Canino'), (4,'Vehiculo');
 
-
+-- ingreso de coordinadoras
 INSERT INTO `televecinosDB`.`Usuario` (
   `nombre`, 
   `apellido`, 
@@ -121,36 +121,33 @@ INSERT INTO `televecinosDB`.`Usuario` (
   `Rol_idRol`, 
   `isBan`
 ) VALUES
-('César', 'Ramírez', '22345678', 'Avenida Patriotas 234, José de La Riva Agüero', 'cesar.ramirez@example.com', 'password2', 2, 1, 'avatar', 2, 2, 0),
-('María', 'Fernández', '32345678', 'Jirón Amazonas 341, Juan XXIII', 'maria.fernandez@example.com', 'password3', 1, 1, 'avatar', 3, 1, 0),
-('Ana', 'Rodríguez', '42345678', 'Calle Arica 452, Libertad', 'ana.rodriguez@example.com', 'password4', 2, 1, 'avatar', 4, 2, 0),
-('Pedro', 'Martínez', '52345678', 'Avenida Ayacucho 563, Los Jardines de La Marina', 'pedro.martinez@example.com', 'password5', 1, 1, 'avatar', 5, 1, 0),
-('Juan', 'López', '62345678', 'Jirón Chancay 674, Las Leyendas', 'juan.lopez@example.com', 'password6',  2, 1, 'avatar', 6, 2, 0),
-('Carlos', 'González', '72345678', 'Calle Castilla 785, Las Torres San Miguelito', 'carlos.gonzalez@example.com', 'password7',  1, 1, 'avatar',1, 1, 0),
-('Sofía', 'Pérez', '82345678', 'Avenida de Los Precursores 896, Elmer Faucett', 'sofia.perez@example.com', 'password8',  2, 1, 'avatar', 1, 2, 0),
-('Lucía', 'Díaz', '92345678', 'Jirón Puno 907, Maranga', 'lucia.diaz@example.com', 'password9', 1, 1, 'avatar', 1, 1, 0),
-('Miguel', 'Hernández', '10345678', 'Calle Junín 1018, Pando', 'miguel.hernandez@example.com', 'password10', 2, 1, 'avatar', 1, 2, 0),
-('Elena', 'Jiménez', '11345678', 'Avenida Bertoloto 1129, Parques de La Huaca', 'elena.jimenez@example.com', 'password11', 1, 1, 'avatar', 1, 1, 0),
-('Valeria', 'Ruiz', '12345679', 'Jirón Tumbes 1240, Otro', 'valeria.ruiz@example.com', 'password12', 2, 1, 'avatar', 1, 2, 0),
-('Diego', 'Morales', '13345678', 'Calle Libertad 1351, Rafael Escardó', 'diego.morales@example.com', 'password13',  1, 1, 'avatar', 1, 1, 0);
+('César', 'Ramírez', '22345678', 'Avenida Patriotas 234, José de La Riva Agüero', 'cesar.ramirez@example.com', 'password2', 1, 1, 'avatar', 2, 3, 0),
+('María', 'Fernández', '32345678', 'Jirón Amazonas 341, Juan XXIII', 'maria.fernandez@example.com', 'password3', 1, 1, 'avatar', 3, 3, 0),
+('Ana', 'Rodríguez', '42345678', 'Calle Arica 452, Libertad', 'ana.rodriguez@example.com', 'password4', 1, 1, 'avatar', 4, 3, 0),
+('Pedro', 'Martínez', '52345678', 'Avenida Ayacucho 563, Los Jardines de La Marina', 'pedro.martinez@example.com', 'password5', 2, 1, 'avatar', 5, 3, 0),
+('Juan', 'López', '62345678', 'Jirón Chancay 674, Las Leyendas', 'juan.lopez@example.com', 'password6',  2, 1, 'avatar', 6, 3, 0);
+-- ingreso de vecinos
+INSERT INTO `televecinosDB`.`Usuario` (
+  `nombre`, 
+  `apellido`, 
+  `dni`, 
+  `direccion`, 
+  `correo`, 
+  `contrasena`, 
+  `avatar`, 
+  `PreguntasFrecuentes_idtable2`, 
+  `Rol_idRol`, 
+  `isBan`
+) VALUES
+('Carlos', 'González', '72345678', 'Calle Castilla 785, Las Torres San Miguelito', 'carlos.gonzalez@example.com', 'password7',  'avatar',1, 2, 0),
+('Sofía', 'Pérez', '82345678', 'Avenida de Los Precursores 896, Elmer Faucett', 'sofia.perez@example.com', 'password8', 'avatar', 1, 2, 0),
+('Lucía', 'Díaz', '92345678', 'Jirón Puno 907, Maranga', 'lucia.diaz@example.com', 'password9', 'avatar', 1, 2, 0),
+('Miguel', 'Hernández', '10345678', 'Calle Junín 1018, Pando', 'miguel.hernandez@example.com', 'password10', 'avatar', 1, 2, 0),
+('Elena', 'Jiménez', '11345678', 'Avenida Bertoloto 1129, Parques de La Huaca', 'elena.jimenez@example.com', 'password11','avatar', 1, 2, 0),
+('Valeria', 'Ruiz', '12345679', 'Jirón Tumbes 1240, Otro', 'valeria.ruiz@example.com', 'password12','avatar', 1, 1, 0),
+('Diego', 'Morales', '13345678', 'Calle Libertad 1351, Rafael Escardó', 'diego.morales@example.com', 'password13', 'avatar', 1, 1, 0);
 
 
-INSERT INTO `televecinosDB`.`Eventos` 
-(`nombre`, `descripcion`, `lugar`, `Coordinador_idUsuario`, `fecha_inicio`, `fecha_fin`, `cantidadVacantes`, `cantDisponibles`, `foto`, `listaMateriales`, `EventEstados_idEventEstados`, `EventFrecuencia_idEventFrecuencia`, `TipoEvento_idTipoEvento`, `ProfesoresEvento_idProfesoresEvento`) VALUES
-('Conferencia Cultural sobre Historia del Arte', 'Una conferencia detallada sobre la evolución del arte a lo largo de los siglos', 'Museo Larco', 1, '2024-06-01 10:00:00', '2024-06-01 12:00:00', 100, 100, 'imagen1.jpg', 'Proyector, Apuntes', 2, 1, 1, 1),
-('Seminario de Arte Moderno', 'Exploración de las tendencias del arte moderno', 'Casa de la Cultura', 2, '2024-06-02 14:00:00', '2024-06-02 16:00:00', 50, 50, 'default.jpg', 'Bloc de notas, Lápices', 3, 2, 1, 2),
-('Taller de Escritura Creativa', 'Desarrolla tus habilidades en escritura creativa', 'Biblioteca Municipal de San Miguel', 3, '2024-06-03 09:00:00', '2024-06-03 11:00:00', 30, 30, 'default.jpg', 'Cuadernos, Plumas', 1, 1, 1, 3),
-('Webinar de Literatura Clásica', 'Discusión en línea sobre obras literarias clásicas', 'Online', 4, '2024-06-04 17:00:00', '2024-06-04 18:30:00', 200, 200, 'default.jpg', 'Computadora, Internet', 3, 1, 1, 4),
-('Foro de Filosofía Contemporánea', 'Debate sobre temas de filosofía contemporánea', 'Centro Cultural de San Miguel', 5, '2024-06-05 10:00:00', '2024-06-05 12:00:00', 40, 40, 'default.jpg', 'Apuntes, Libros', 1, 1, 1, 5),
-('Mesa Redonda de Historia Mundial', 'Discusión sobre eventos históricos importantes', 'Auditorio Municipal', 6, '2024-06-06 14:00:00', '2024-06-06 16:00:00', 60, 60, 'default.jpg', 'Micrófonos, Proyector', 3, 1, 1, 6);
-
--- Inserciones para la tabla Eventos (Eventos Deportivos)
-INSERT INTO `televecinosDB`.`Eventos` 
-(`nombre`, `descripcion`, `lugar`, `Coordinador_idUsuario`, `fecha_inicio`, `fecha_fin`, `cantidadVacantes`, `cantDisponibles`, `foto`, `listaMateriales`, `EventEstados_idEventEstados`, `EventFrecuencia_idEventFrecuencia`, `TipoEvento_idTipoEvento`, `ProfesoresEvento_idProfesoresEvento`) VALUES
-('Conferencia Deportiva sobre Fútbol', 'Una conferencia sobre estrategias y tácticas en fútbol', 'Estadio San Miguel',4, '2024-06-01 15:00:00', '2024-06-01 17:00:00', 100, 100, 'default.jpg', 'Proyector, Apuntes', 2, 1, 2, 11),
-('Seminario de Nutrición Deportiva', 'Discusión sobre la importancia de la nutrición en el deporte', 'Casa del Deporte', 3, '2024-06-02 10:00:00', '2024-06-02 12:00:00', 50, 50, 'default.jpg', 'Bloc de notas, Lápices', 3, 1, 2, 12),
-('Taller de Entrenamiento Funcional', 'Entrenamiento intensivo para mejorar la condición física', 'Gimnasio Municipal', 2, '2024-06-03 08:00:00', '2024-06-03 10:00:00', 30, 30, 'default.jpg', 'Ropa deportiva, Agua', 1, 1, 2, 13),
-('Webinar de Psicología Deportiva', 'Conferencia en línea sobre la psicología aplicada al deporte', 'Online', 1, '2024-06-04 17:00:00', '2024-06-04 18:30:00', 200, 200, 'default.jpg', 'Computadora, Internet', 1, 1, 2, 14);
 
 
 INSERT INTO `televecinosDB`.`Usuario` (
@@ -173,31 +170,22 @@ INSERT INTO `televecinosDB`.`Serenazgo` (`numTelefono`, `fechaNacimiento`, `Turn
 ( '923456789', '1992-02-02', 2, 2,14),
 ( '934567890', '1994-03-03', 1, 3,15);
 
-INSERT INTO `televecinosDB`.`Incidencias` (
-  `nombreIncidencia`, 
-  `fecha`, 
-  `lugarExacto`, 
-  `urbanizacion_idUrbanizacion`,
-  `referencia`, 
-  `foto`, 
-  `ambulancia`, 
-  `numeroContacto`, 
-  `CriticidadIncidencia_idCriticidadIncidencia`, 
-  `TipoIncidencia_idTipoIncidencia`, 
-  `EstadosIncidencia_idEstadosIncidencia`, 
-  `Serenazgo_idSerenazgo`, 
-  `Usuario_idUsuario`, 
-  `incidenciaPersonal`
-) VALUES
-('Robo', '2024-01-01 12:00:00', 'Av. La Marina 1234',1, 'Cerca de la Plaza San Miguel', 'foto_1.jpg', 0, '987654321', 1, 1, 1, 1, 1, 0),
-('Asalto', '2024-01-02 13:00:00', 'Av. Universitaria 5678',3, 'Frente al Centro Comercial Plaza San Miguel', 'foto_2.jpg', 1, '987654322', 2, 2, 2, 2, 2, 0),
-('Accidente de Tránsito', '2024-01-03 14:00:00', 'Jirón Pescadores 123',5, 'Intersección con Av. La Marina', 'foto_3.jpg', 1, '987654323', 3, 3, 3, 3, 3, 0),
-('Incendio', '2024-01-04 15:00:00', 'Calle Bolognesi 456',7, 'Edificio Residencial', 'foto_4.jpg', 1, '987654324', 1, 4, 4, 3, 4, 0),
-('Emergencia Médica', '2024-01-05 16:00:00', 'Av. Bertolotto 789',9, 'Cerca del Hospital San José', 'foto_5.jpg', 0, '987654325', 2, 1, 5, 2, 5, 1),
-('Violencia Doméstica', '2024-01-06 17:00:00', 'Jirón Mantaro 101',11, 'Vivienda Unifamiliar', 'foto_6.jpg', 0, '987654326', 3, 1, 2, 1, 6, 1),
-('Robo', '2024-01-07 18:00:00', 'Calle Salaverry 102',12, 'En la esquina', 'foto_7.jpg', 0, '987654327', 1, 3,2, 2, 7, 0),
-('Asalto', '2024-01-08 19:00:00', 'Av. Faucett 103',10, 'Cerca de la escuela', 'foto_8.jpg', 1, '987654328', 2, 4,3, 3, 8, 0);
+INSERT INTO `televecinosDB`.`Eventos` 
+(`nombre`, `descripcion`, `lugar`, `Coordinador_idUsuario`, `fecha_inicio`, `fecha_fin`, `cantidadVacantes`, `cantDisponibles`, `foto`, `listaMateriales`, `EventEstados_idEventEstados`, `EventFrecuencia_idEventFrecuencia`, `TipoEvento_idTipoEvento`, `ProfesoresEvento_idProfesoresEvento`) VALUES
+('Conferencia Cultural sobre Historia del Arte', 'Una conferencia detallada sobre la evolución del arte a lo largo de los siglos', 'Museo Larco', 1, '2024-06-01 10:00:00', '2024-06-01 12:00:00', 100, 100, 'imagen1.jpg', 'Proyector, Apuntes', 2, 1, 1, 1),
+('Seminario de Arte Moderno', 'Exploración de las tendencias del arte moderno', 'Casa de la Cultura', 2, '2024-06-02 14:00:00', '2024-06-02 16:00:00', 50, 50, 'default.jpg', 'Bloc de notas, Lápices', 3, 2, 1, 2),
+('Taller de Escritura Creativa', 'Desarrolla tus habilidades en escritura creativa', 'Biblioteca Municipal de San Miguel', 3, '2024-06-03 09:00:00', '2024-06-03 11:00:00', 30, 30, 'default.jpg', 'Cuadernos, Plumas', 1, 1, 1, 3),
+('Webinar de Literatura Clásica', 'Discusión en línea sobre obras literarias clásicas', 'Online', 1, '2024-06-04 17:00:00', '2024-06-04 18:30:00', 200, 200, 'default.jpg', 'Computadora, Internet', 3, 1, 1, 4),
+('Foro de Filosofía Contemporánea', 'Debate sobre temas de filosofía contemporánea', 'Centro Cultural de San Miguel',2, '2024-06-05 10:00:00', '2024-06-05 12:00:00', 40, 40, 'default.jpg', 'Apuntes, Libros', 1, 1, 1, 5),
+('Mesa Redonda de Historia Mundial', 'Discusión sobre eventos históricos importantes', 'Auditorio Municipal', 3, '2024-06-06 14:00:00', '2024-06-06 16:00:00', 60, 60, 'default.jpg', 'Micrófonos, Proyector', 3, 1, 1, 6);
 
+-- Inserciones para la tabla Eventos (Eventos Deportivos)
+INSERT INTO `televecinosDB`.`Eventos` 
+(`nombre`, `descripcion`, `lugar`, `Coordinador_idUsuario`, `fecha_inicio`, `fecha_fin`, `cantidadVacantes`, `cantDisponibles`, `foto`, `listaMateriales`, `EventEstados_idEventEstados`, `EventFrecuencia_idEventFrecuencia`, `TipoEvento_idTipoEvento`, `ProfesoresEvento_idProfesoresEvento`) VALUES
+('Conferencia Deportiva sobre Fútbol', 'Una conferencia sobre estrategias y tácticas en fútbol', 'Estadio San Miguel',4, '2024-06-01 15:00:00', '2024-06-01 17:00:00', 100, 100, 'default.jpg', 'Proyector, Apuntes', 2, 1, 2, 11),
+('Seminario de Nutrición Deportiva', 'Discusión sobre la importancia de la nutrición en el deporte', 'Casa del Deporte', 4, '2024-06-02 10:00:00', '2024-06-02 12:00:00', 50, 50, 'default.jpg', 'Bloc de notas, Lápices', 3, 1, 2, 12),
+('Taller de Entrenamiento Funcional', 'Entrenamiento intensivo para mejorar la condición física', 'Gimnasio Municipal', 5, '2024-06-03 08:00:00', '2024-06-03 10:00:00', 30, 30, 'default.jpg', 'Ropa deportiva, Agua', 1, 1, 2, 13),
+('Webinar de Psicología Deportiva', 'Conferencia en línea sobre la psicología aplicada al deporte', 'Online', 5, '2024-06-04 17:00:00', '2024-06-04 18:30:00', 200, 200, 'default.jpg', 'Computadora, Internet', 1, 1, 2, 14);
 
 
 
