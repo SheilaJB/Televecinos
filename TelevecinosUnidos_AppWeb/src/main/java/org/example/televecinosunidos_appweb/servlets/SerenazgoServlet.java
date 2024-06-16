5b93bc4719d68e2567592ce347b2f024a1b2cdeb
Jesus Alvarado <a20211688@pucp.edu.pe>
2024-06-16 12:23:22 -0500

    avance vista incidencia desde Serenazgo

--- TelevecinosUnidos_AppWeb/src/main/java/org/example/televecinosunidos_appweb/servlets/SerenazgoServlet.java
+++ TelevecinosUnidos_AppWeb/src/main/java/org/example/televecinosunidos_appweb/servlets/SerenazgoServlet.java
@@ -3,6 +3,7 @@ package org.example.televecinosunidos_appweb.servlets;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import org.example.televecinosunidos_appweb.model.daos.IncidenciaDao;
 
 import java.io.IOException;
 
@@ -10,10 +11,16 @@ import java.io.IOException;
 public class SerenazgoServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        IncidenciaDao incidenciaDao = new IncidenciaDao();
+
         String action = request.getParameter("action")==null?"inicioSerenazgo":request.getParameter("action");
-
+        String vista;
         switch (action) {
-
+            case "listaIncidencias_S":
+                vista = "WEB-INF/Serenazgo/listaIncidencias_S.jsp";
+                request.setAttribute("lista",incidenciaDao.listarIncidencias());
+                request.getRequestDispatcher(vista).forward(request, response);
+                break;
             default:
                 request.getRequestDispatcher("WEB-INF/Serenazgo/" + action + ".jsp").forward(request,response);
         }
